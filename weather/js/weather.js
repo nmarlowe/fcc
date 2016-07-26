b832b2f2acfec3fe7d1526fc292993fbe23a6c33
Neil Marlowe <nmarlowe@gmail.com>
2016-07-25 22:56:50 -0400

    add color to high and low arrows

--- weather/js/weather.js
+++ weather/js/weather.js
@@ -10,7 +10,7 @@ function loadWeather(pos) {
       html = '<h2>Current Weather For '+weather.city+', '+weather.region+'</h2>';
       html += '<h4><i class="icon-'+weather.code+'"></i>'+weather.currently+'</h4>';
       html += '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2><br>';
-      html += '<h4><i class="fa fa-arrow-up fa-1x"></i>&nbsp;'+weather.high+'&nbsp;&nbsp;<i class="fa fa-arrow-down fa-1x"></i>&nbsp;'+weather.low+'</h4>';
+      html += '<h4><i class="fa fa-arrow-up fa-1x high"></i>&nbsp;'+weather.high+'&nbsp;&nbsp;<i class="fa fa-arrow-down fa-1x low"></i>&nbsp;'+weather.low+'</h4>';
 
       $("#weather").html(html);
 
