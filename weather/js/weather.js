cb3b6e9e4d18976855baaced3e776fc88f94c91e
Neil Marlowe <nmarlowe@gmail.com>
2016-07-25 22:38:24 -0400

    Styling and custom weather font

--- weather/js/weather.js
+++ weather/js/weather.js
@@ -7,16 +7,19 @@ function loadWeather(pos) {
       console.log(weather);
       //document.getElementById("temp").innerHTML += weather.temp;
 
-      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<img src="'+weather.image+'">';
+      html = '<h2>Current Weather For '+weather.city+', '+weather.region+'</h2>';
+      html += '<h4><i class="icon-'+weather.code+'"></i>'+weather.currently+'</h4>';
+      html += '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2><br>';
+      html += '<h4><i class="fa fa-arrow-up fa-1x"></i>&nbsp;'+weather.high+'&nbsp;&nbsp;<i class="fa fa-arrow-down fa-1x"></i>&nbsp;'+weather.low+'</h4>';
 
       $("#weather").html(html);
 
     },
     error: function(error) {
       console.log(error);
+      html = '<h2>'+error+'</h2>';
+
+      $("#weather").html(html);
     }
   });
 }
