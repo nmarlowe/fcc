4f46548646646eb98fcdd43da91f99b1689505c3
Neil Marlowe <nmarlowe@gmail.com>
2016-07-26 22:59:04 -0400

    add logic for no location permission

--- weather/js/location.js
+++ weather/js/location.js
@@ -1,7 +1,30 @@
 
-if (navigator.geolocation) {
+// if ("geolocation" in navigator) {
+//   console.log("Geo Yes");
+//   navigator.geolocation.getCurrentPosition(function(position, error) {
+//     console.log(position.coords.latitude + "," + position.coords.longitude)
+//     loadWeather(position.coords.latitude + "," + position.coords.longitude);
+//   },
+//   function(error){
+//     if (error.code == error.PERMISSION_DENIED) {
+//       html = '<h2>Geolocation is not available in your browser</h2>'
+//     }
+//   });
+// }
+
+if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position.coords.latitude + "," + position.coords.longitude)
-    loadWeather(position.coords.latitude + "," + position.coords.longitude);
+    console.log(position.coords.latitude + "," + position.coords.longitude);
+    loadWeather(position.coords.latitude + "," + position.coords.longitude)
+  },
+  function (error) {
+    if (error.code == error.PERMISSION_DENIED)
+        console.log("you denied me :-(");
+        html = '<h2>You must allow location services in your browser</h2>';
+        $("#weather").html(html);
   });
+} else {
+  console.log("Geolocation not supported in your browser");
+  html = '<h2>Geolocation is not supported in your browser</h2>';
+  $("#weather").html(html);
 }
--- weather/js/weather.js
+++ weather/js/weather.js
@@ -5,15 +5,12 @@ function loadWeather(pos) {
     units: "f",
     success: function(weather) {
       console.log(weather);
-      //document.getElementById("temp").innerHTML += weather.temp;
 
       html = '<h2>Current Weather For '+weather.city+', '+weather.region+'</h2>';
       html += '<h4><i class="icon-'+weather.code+'"></i>&nbsp;'+weather.currently+'</h4>';
       html += '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<h4>Today: &nbsp;<i class="icon-'+weather.forecast[0].code+'"></i>&nbsp;'+weather.forecast[0].text+'</h4><br>';
       html += '<h4><i class="fa fa-arrow-up fa-1x high"></i>&nbsp;'+weather.high+'&nbsp;&nbsp;<i class="fa fa-arrow-down fa-1x low"></i>&nbsp;'+weather.low+'</h4>';
-      // html += '<h4>Tomorrow&#39;s Forecast: &nbsp;<i class="icon-'+weather.forecast[1].code+'"></i>&nbsp;'+weather.forecast[1].text+'</h4><br>';
-      // html += '<h4><i class="fa fa-arrow-up fa-1x high"></i>&nbsp;'+weather.forecast[1].high+'&nbsp;&nbsp;<i class="fa fa-arrow-down fa-1x low"></i>&nbsp;'+weather.forecast[1].low+'</h4>';
 
       $("#weather").html(html);
 
