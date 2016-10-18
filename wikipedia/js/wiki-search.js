9493e738f68b7cd62728a57a00c51ea7d5066fa0
Neil Marlowe <nmarlowe@gmail.com>
2016-10-17 22:04:17 -0400

    Search returns 1 result now

--- wikipedia/js/wiki-search.js
+++ wikipedia/js/wiki-search.js
@@ -1,28 +1,38 @@
 // searchTerms = word1+word2
-var searchTerms;
+var searchTerms = "";
+var searchUrl = "";
 
-var settings = {
+var searchSettings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=1&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + searchTerms,
+    "url": searchUrl,
     "type": "GET"
 };
+
+function searchWikipedia(url) {
+  $.ajax(url).done(function(response) {
+    console.log(searchSettings.url);
+    var pageId = response.query.pages[0].pageid;
+    var title = response.query.pages[0].title;
+    var pageLink = "https://en.wikipedia.org/?curid=" + pageId;
+    console.log(title);
+    $("#results").empty().append('<div class="select"><p>Select one of the following:</p></div>');
+    $("#results").append('<ol><li><a class="menu-item" href="' + pageLink + '">' + title + '</a></li></ol>');
+  });
+}
+
 function readInput(el, e) {
-  if (e.keyCode == 13) {
+  if (e.keyCode === 13) {
     searchTerms = el.value;
-    searchWikipedia();
+    searchUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=%2a&prop=extracts&generator=search&formatversion=latest&exsentences=1&exlimit=1&exintro=1&gsrsearch=" + searchTerms;
+    console.log(searchUrl);
+    searchWikipedia(searchUrl);
   }
 }
 
 function startSearch() {
   $("#first-screen").hide();
-  $("#results").empty().append('What would you like to search for? ===> <input type="search" id="searchInput" name="search" onkeydown="readInput(this, event)" autofocus>');
+  $("#results").empty().append('What would you like to search for? ===> <input type="text" id="searchInput" name="search" onkeydown="readInput(this, event)" autofocus>');
 
-}
 
-function searchWikipedia() {
-  $.ajax(settings).done(function(response) {
-    var title = response.query.pages.title;
-    console.log(title);
-  });
 }
