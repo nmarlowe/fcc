3f94c3d664b8527624330f0705a68e72b9ffb852
nmarlowe <nmarlowe@gmail.com>
2016-10-27 16:15:30 -0400

    Add dithering and image retrieval

--- wikipedia/js/wiki-random.js
+++ wikipedia/js/wiki-random.js
@@ -2,7 +2,7 @@
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&origin=%2A&prop=extracts&generator=random&exsentences=10&grnnamespace=0&indexpageids=",
+    "url": "https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&piprop=original%7Cname&origin=%2A&prop=extracts%7Cpageimages&generator=random&exsentences=10&grnnamespace=0&indexpageids=",
     "type": "GET"
 };
 
@@ -11,13 +11,16 @@ function randomArticle() {
       var pageID = "";
       var title = "";
       var extract = "";
+      var pageImage = "";
 
       pageID = response.query.pageids[0];
       title = response.query.pages[0].title;
       extract = response.query.pages[0].extract;
+      pageImage = response.query.pages[0].thumbnail["original"];
       console.log("Page ID: " + pageID);
       console.log("Title: " + title);
       console.log("Extract: " + extract);
+      console.log("Page Images: " + pageImage);
 
       $("#first-screen").hide();
       $("#results").empty().append('Page Title: ' + title + '<br><br>' + 'Page Intro: ' + extract + '<br><br>' + 'Would you like to go <a href="#" id="continue-next" class="intro-menu">[p]roceed</a> to the page or go back to the <a href="#" id="back" class="intro-menu">[m]ain</a> to the menu? ===> <span class="cursor">_</span>');
